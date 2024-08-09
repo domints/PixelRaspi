72a8d39d957d4e33843393cb330c3c3ef9838a16
Dominik Szymański <domints@gmail.com>
2024-08-10 00:30:08 +0200

    Add setup scripts

--- setup.sh
+++ setup.sh
@@ -5,7 +5,11 @@ if [[ $EUID -ne 0 ]]; then
     exit 100
 fi
 
+chown -R hskrk:hsrk .
 chmod +x update.sh
+
+sudo python -m venv .venv
+pip install -r requirements.txt
 
 cp flipdot.service /etc/systemd/system/flipdot.service
 systemctl daemon-reload
